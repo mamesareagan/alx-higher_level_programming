c785035b3f6338afa4ea22fd108d60ff6251f58c
mamesareagan <mamesareagan0@gmai.com>
2023-06-06 11:37:58 -0700

    checked pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''program that prints the ASCII alphabet, in lowercase, not followed by a new line.'''
+'''program that prints the ASCII alphabet
+in lowercase
+not followed by a new line.'''
 for i in range(97, 123):
     if (chr(i) == 'q') or (chr(i) == 'e'):
         continue
