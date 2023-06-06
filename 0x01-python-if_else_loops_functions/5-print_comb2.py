52af26f5e613ef431cb48362e12e31d3239933e0
mamesareagan <mamesareagan0@gmai.com>
2023-06-06 12:43:57 -0700

    prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#  program that prints numbers from 0 to 99
+for i in range(100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}, ".format(i), end='')
