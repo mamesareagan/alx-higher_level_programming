0769d901087d845fa0dad6aac9636a9c833d5ffd
mamesareagan <mamesareagan0@gmai.com>
2023-06-10 19:43:35 -0700

    added documentation

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    ''' replaces an element in a list 
+        at a specific position 
+        without modifying the original list'''
     if idx < 0 or (idx > (len(my_list) - 1)):
         return (my_list)
     new = [x for x in my_list]
