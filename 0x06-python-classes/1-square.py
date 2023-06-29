7aba2223295fd5ebaeef7afd0c2f7cc621a4be64
mamesareagan <mamesareagan0@gmai.com>
2023-06-28 18:11:24 -0700

    added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    """Represent a square."""
+"""Represent a square.
+private instance of  size"""
 
+
+class Square:
     def __init__(self, size):
         """Initialize a new Square.
 
