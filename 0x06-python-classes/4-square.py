4836a471d4b88b2b0d8870c892c20483e4628946
mamesareagan <mamesareagan0@gmai.com>
2023-06-28 18:44:53 -0700

    setter and getter of size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        '''Sets value of __size.
+
+        Args:
+            value (int): assignment value for __size.
+        Raises:
+            TypeError: if 'size' is not an integer.
+            ValueError: if 'size' is less than zero.
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
