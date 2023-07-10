78482c88fd1fd15c6242e50e2d2b6b921e58406f
mamesareagan <mamesareagan0@gmai.com>
2023-07-10 13:11:24 -0700

    checks if an object inherits a class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    '''checks if object is subclass of a_class
+
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The super class.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    '''
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return (True)
+    return(False)
