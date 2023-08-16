697b591d52e30e2ca057a967b8144547defb3d89
mamesareagan <mamesareagan0@gmai.com>
2023-08-16 14:48:50 -0700

    creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (
+        id INT AUTO_INCREMENT NOT NULL,
+        name VARCHAR(256),
+        PRIMARY KEY (id)
+);
