b246db0961008c47f4ce919958226666292e3409
mamesareagan <mamesareagan0@gmai.com>
2023-08-16 13:15:50 -0700

    lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- show grants for user user_0d_1
+-- show grants for user user_0d_1, user_0d_1
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
-
--- show grants for user user_0d_1
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
