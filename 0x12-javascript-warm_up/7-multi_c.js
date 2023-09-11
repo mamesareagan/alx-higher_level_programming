2ee874d23060aa50fc7de4a0dd55ba46ada2f841
mamesareagan <mamesareagan0@gmail.com>
2023-09-11 21:23:36 +0300

    prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = Math.floor(Number(process.argv[2]));
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
