e3a42d5c531aaddff57fdb1f3d8d5863eb8f89a0
mamesareagan <mamesareagan0@gmail.com>
2023-09-11 21:06:00 +0300

    loooping through an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const sentences = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let count = 0;
+while (sentences[count]) {
+  console.log(sentences[count]);
+  count++;
+}
