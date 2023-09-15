4775ed36cf8fa2ce561889e3d1034b7bf38bf830
mamesareagan <mamesareagan0@gmail.com>
2023-09-15 15:20:35 +0300

    lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
 # Lists all states from the database hbtn_0e_0_usa.
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+import sys 
+import MySQLdb
 
-    usr = sys.argv[1]
-    pas = sys.argv[2]
-    dtb = sys.argv[3]
+usr = sys.argv[1]
+pas = sys.argv[2]
+dtb = sys.argv[3]
+
+if __name__ == "__main__":
     db = MySQLdb.connect(user=usr, passwd=pas, db=dtb)
     c = db.cursor()
     c.execute("SELECT * FROM `states`ORDER BY `id`")
