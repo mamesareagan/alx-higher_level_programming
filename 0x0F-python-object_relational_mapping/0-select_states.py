304c9497e309f53337528d51fd6f10540beb2ea5
mamesareagan <mamesareagan0@gmail.com>
2023-09-15 15:08:23 +0300

    ordering by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     dtb = sys.argv[3]
     db = MySQLdb.connect(user=usr, passwd=pas, db=dtb)
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
+    c.execute("SELECT * FROM `states`ORDER BY `id`")
     [print(state) for state in c.fetchall()]
